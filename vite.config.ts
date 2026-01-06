04e98179fb1e902cc0db6b4d0690b3c340b6cc3d
Antti Rautanen <anttirautanen@users.noreply.github.com>
2026-01-06 14:57:07 +0200

    Add configuration

---
+++ vite.config.ts
@@ -0,0 +1,25 @@
+import { dirname, resolve } from "node:path"
+import { fileURLToPath } from "node:url"
+import { defineConfig } from "vite"
+import viteReact from "@vitejs/plugin-react"
+
+const __dirname = dirname(fileURLToPath(import.meta.url))
+
+export default defineConfig({
+  plugins: [viteReact()],
+  build: {
+    lib: {
+      entry: resolve(__dirname, "lib/gallery.ts"),
+      name: "Gallery",
+      fileName: "gallery",
+    },
+    rollupOptions: {
+      external: ["react", "react-dom"],
+      output: {
+        globals: {
+          react: "React",
+        },
+      },
+    },
+  },
+})
