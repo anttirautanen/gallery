fc9b8e7c340fa7013a0430798ae4f9aee7a4e3d2
Antti Rautanen <anttirautanen@users.noreply.github.com>
2026-01-06 16:23:35 +0200

    Export missing type

--- dist/gallery.d.ts
+++ dist/gallery.d.ts
@@ -1,3 +1,3 @@
 import { Gallery } from "./gallery/Gallery.js";
-export type { GalleryDescription } from "./gallery/types.js";
+export type { GalleryDescription, GalleryDescriptionWithPath } from "./gallery/types.js";
 export { Gallery };
